6c800e4cf89abc17cdb08e18236412b25f502873
pranaya-sht <pranayashrestha8888@gmail.com>
2024-07-13 20:16:28 +0545

    graph implementation

--- graph.h
+++ graph.h
@@ -17,7 +17,13 @@ public:
     void addEdge(int vertex1, int vertex2);
     void removeVertex(int vertex);
     void removeEdge(int vertex1, int vertex2);
-    
+    int numVertices() const;
+    int numEdges() const;
+    int indegree(int vertex) const;
+    int outdegree(int vertex) const;
+    int degree(int vertex) const;
+    std::vector<int> neighbours(int vertex) const;
+    bool neighbour(int vertex1, int vertex2) const;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -20,13 +20,32 @@ int main() {
     // Display graph properties
     std::cout << "Graph is empty: " << g.isEmpty() << std::endl;
     std::cout << "Graph is directed: " << g.isDirected() << std::endl;
-    
+    std::cout << "Number of vertices: " << g.numVertices() << std::endl;
+    std::cout << "Number of edges: " << g.numEdges() << std::endl;
 
-    
+    // Display degree information
+    std::cout << "Indegree of vertex 1: " << g.indegree(1) << std::endl;
+    std::cout << "Outdegree of vertex 1: " << g.outdegree(1) << std::endl;
+    std::cout << "Degree of vertex 1: " << g.degree(1) << std::endl;
 
-    
+    // Display neighbors of a vertex
+    std::vector<int> neighbors = g.neighbours(1);
+    std::cout << "Neighbors of vertex 1: ";
+    for (int v : neighbors) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
 
-    
+    // Check if vertices are neighbors
+    std::cout << "Vertex 1 is neighbor of vertex 0: " << g.neighbour(1, 0) << std::endl;
+
+    // Remove edge
+    g.removeEdge(1, 2);
+    std::cout << "Number of edges after removing edge (1,2): " << g.numEdges() << std::endl;
+
+    // Remove vertex
+    g.removeVertex(2);
+    std::cout << "Number of vertices after removing vertex 2: " << g.numVertices() << std::endl;
 
     return 0;
 }
